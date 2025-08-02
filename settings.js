32a47b87f9c7ae5debd7ead1779ac8b8c7d0ae36
FF3X <d4warrior12345@gmail.com>
2025-08-02 22:12:46 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 module.exports = {
-  SESSION_ID: "", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
+  SESSION_ID: "", // Put Session Id Here Inside "SUBZERO-MD;;;SBM03Y7I#Kp_OAndpc2WpkgKY8qgWMR3W27UuXdWbwZ-3uEOELWE"
+  OWNER_NUMBER: "2347085352705", // Put Owner Number Here
   PREFIX: "." // Default Prefix You Can Change To .,*!?+-&%#@
 };
 
